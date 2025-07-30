ade8457e264e822085f1e02f1d6b0e325dc5e189
Iranlsjr <iran.lourenco.jr@gmail.com>
2025-07-30 11:43:27 -0300

    Primeiros Passos

---
+++ GFT Start #7 .NET/2-sintaxe-basica-com.NET/ExemplosFundamentos/Program.cs
@@ -0,0 +1,30 @@
+﻿using ExemplosFundamentos.Models;
+
+//classes -------
+//Pessoa pessoa1 = new Pessoa();
+
+//pessoa1.Nome = "Iran";
+//pessoa1.Idade = 32;
+//pessoa1.Apresentar();
+
+//Variáveis ---------
+//string apresentacao = "Olá, seja bem vindo";
+//int quantidade = 1;
+//quantidade = 10;
+//double altura = 1.80;
+//decimal preco = 1.80M;
+//bool condicao = true;
+
+//Console.WriteLine(apresentacao);
+//Console.WriteLine("Valor da variável quantidade: " + quantidade);
+//Console.WriteLine("Valor da variável altura: " + altura.ToString("0.00"));
+//Console.WriteLine("Valor da variável preço: " + preco);
+//Console.WriteLine("Valor da variável condição: " + condicao);
+
+//Date Time
+DateTime dataAtual = DateTime.Now;
+Console.WriteLine(dataAtual);
+Console.WriteLine(dataAtual.ToString("dd/MM/yyyy HH:mm"));
+//adicionando mais dias
+DateTime dataAtualDias = DateTime.Now.AddDays(5);
+Console.WriteLine(dataAtualDias.ToString("dd/MM/yyyy"));
